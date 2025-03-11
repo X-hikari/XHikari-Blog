8e36863d06e5e6cd5872d5fc33d49f3c39b3443a
X-hikari <xinyu8632@gmail.com>
2025-03-11 13:31:06 +0800

    留言修改和删除缓存

--- README.md
+++
@@ -1,3 +0,0 @@
-# XHikari-Blog
-
-这里是XHikari的个人博客(正在计划开发)
--- fronted/src/admin.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import AdminApp from './AdminApp.vue';
-// import adminRouter from './router/admin';
-
-const app = createApp(AdminApp)
-// app.use(adminRouter)
-app.mount("#admin-app")
-
-  
